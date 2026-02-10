5d25afef37268ae68663ad7999f4a284f6030fbc
Rok Prezelj <prezelj.rok@gmail.com>
2026-02-10 22:16:30 +0100

    Added goalie following the ball. Updated the todo list in readme.

---
+++ README.md
@@ -0,0 +1,11 @@
+# About
+We are the rolling balls.
+![logo](logo.png)
+
+# TODOs
+- [ ] TODO: Shoot ball.
+- [ ] TODO: Ball posessions checker.
+- [ ] TODO: Find closest player to ball.
+- [ ] TODO: Predict ball future position, including wall bounce and corner hill.
+- [ ] TODO: Shot blocker calculator.
+- [x] TODO: Goalie follows ball.
---
+++ follow_ball.py
@@ -0,0 +1,38 @@
+import time
+
+import requests
+
+s = requests.Session()
+base = "http://localhost:8080"
+
+while True:
+    try:
+        state = s.get(f"{base}/State", headers={"accept": "application/json"}).json()
+        cam0 = state["camData"][0]
+
+        ball_y = float(cam0["ball_y"])  # y coordinate of the ball in milimeters.
+
+        # Ball is outside the field
+        if ball_y < 0:
+            print("ball not detected", ball_y)
+            time.sleep(1 / 50)
+            continue
+
+        # Calcualte to goalie_pos: 0 => 245 mm, 1=> 455 mm.
+        goalie_pos = min(max(0.0, (ball_y - 245.0) / 210.0), 1.0)
+
+        payload = {"commands": [{
+            "driveID": 1,  # Goali id
+            "rotationTargetPosition": 0.0,
+            "rotationVelocity": 0.0,
+            "translationTargetPosition": goalie_pos,
+            "translationVelocity": 1.0,  # Max speed
+        }]}
+
+        # Send payload to url.
+        resp = s.post(f"{base}/Motors/SendCommand", json=payload)
+
+        time.sleep(1 / 50)
+
+    except KeyboardInterrupt:
+        break
